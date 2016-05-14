959c944b0e027f5e0eb7be65db8753fb97767650
Sam Schwartz <obct537@gmail.com>
2016-05-14 09:37:14 -0500

    Footer component

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -3,12 +3,52 @@ import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {Http} from '@angular/http';
 import {Header} from '../header/header.component';
 import {View} from '../view/view.component';
+import TitleTile from '../title-tile/title-tile.component';
+import {Header} from '../directives/header.component';
+import {Footer} from '../directives/footer.component';
+/////////////////////////
+// ** Example Directive
+// Notice we don't touch the Element directly
+@Directive({
+  selector: '[x-large]'
+})
+export class XLarge {
+  constructor(element: ElementRef, renderer: Renderer) {
+    // ** IMPORTANT **
+    // we must interact with the dom through -Renderer-
+    // for webworker/server to see the changes
+    renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
+    // ^^
+  }
+}
 
+/////////////////////////
+// ** Example Components
+@Component({
+  selector: 'home',
+  template: `
+    <div>This is the "Home" page</div>
+  `
+})
+export class Home { }
+
+@Component({
+  selector: 'about',
+  template: `
+  <div>This is the "About" page</div>
+  `
+})
+export class About { }
+
+/////////////////////////
+// ** MAIN APP COMPONENT **
 @Component({
   selector: 'app', // <app></app>
   directives: [
     ...ROUTER_DIRECTIVES,
     Header,
+    TitleTile,
+    Footer
   ],
   styles: [
     require('./app.component.css')
