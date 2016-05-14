214fe374edc2abe423446485a465ec99269bc005
alex <gigrithink@gmail.com>
2016-05-14 17:48:11 +0200

    Search component

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
+import {Search} from '../search/search.component';
 
 @Component({
   selector: '.content-header', // <app></app>
-  directives: [],
+  directives: [Search],
   template: require('./header.component.html')
 })
 export class Header {
---
+++ src/components/search/search.component.ts
@@ -0,0 +1,10 @@
+import {Component, Directive, ElementRef, Renderer} from '@angular/core';
+
+@Component({
+  selector: '.plone-search',
+  directives: [],
+  template: require('./search.component.html')
+})
+export class Search {
+  constructor() { }
+}
