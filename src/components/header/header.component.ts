33c0604a0347c445c6d27e0a348616414438882d
Sam Schwartz <obct537@gmail.com>
2016-05-14 11:18:16 -0500

    removed merge conflict

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,11 +1,4 @@
 import {Component} from '@angular/core';
-<<<<<<< HEAD
-import {Search} from '../search/search.component';
-
-@Component({
-  selector: '.content-header', // <app></app>
-  directives: [Search],
-=======
 import {Logo} from '../logo/logo.component';
 import {Search} from '../search/search.component';
 
@@ -15,7 +8,6 @@ import {Search} from '../search/search.component';
     Logo,
     Search
   ],
->>>>>>> cba400c... Added breadcrumb & logo components
   template: require('./header.component.html')
 })
 export class Header {
