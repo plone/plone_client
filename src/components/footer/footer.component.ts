26ab6bfcf932af4503a39035acde946df413ec01
Sam Schwartz <obct537@gmail.com>
2016-05-14 10:00:16 -0500

    added footer component

---
+++ src/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '.plone-footer', // <app></app>
+  directives: [],
+  template: require('./footer.component.html')
+})
+export class Footer {
+  constructor() { }
+
+}
