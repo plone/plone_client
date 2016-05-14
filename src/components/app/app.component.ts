418e5cc5fc59df06941279577220a78e3d3fc353
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-05-14 17:03:31 +0200

    cleanup

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -5,39 +5,7 @@ import {Header} from '../header/header.component';
 import {Footer} from '../footer/footer.component';
 import {View} from '../view/view.component';
 import TitleTile from '../title-tile/title-tile.component';
-/////////////////////////
-// ** Example Directive
-// Notice we don't touch the Element directly
-@Directive({
-  selector: '[x-large]'
-})
-export class XLarge {
-  constructor(element: ElementRef, renderer: Renderer) {
-    // ** IMPORTANT **
-    // we must interact with the dom through -Renderer-
-    // for webworker/server to see the changes
-    renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
-    // ^^
-  }
-}
 
-/////////////////////////
-// ** Example Components
-@Component({
-  selector: 'home',
-  template: `
-    <div>This is the "Home" page</div>
-  `
-})
-export class Home { }
-
-@Component({
-  selector: 'about',
-  template: `
-  <div>This is the "About" page</div>
-  `
-})
-export class About { }
 
 /////////////////////////
 // ** MAIN APP COMPONENT **
