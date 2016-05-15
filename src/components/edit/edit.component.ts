a43ee6415fc9889304aa4b5b2af3157cbac83729
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-05-15 12:18:17 +0200

    be able to edit with api

--- src/components/edit/edit.component.ts
+++ src/components/edit/edit.component.ts
@@ -1,7 +1,8 @@
 import {Component, Renderer} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import TitleTile from '../title-tile/title-tile.component';
-import {RouteParams} from '@angular/router-deprecated';
+import {RouteParams, Router} from '@angular/router-deprecated';
+import {Document} from '../../models/document';
 
 @Component({
   selector: 'edit',
@@ -11,22 +12,55 @@ import {RouteParams} from '@angular/router-deprecated';
   template: require('./edit.component.html')
 })
 export class Edit {
-  data = {};
+  model: Document = {
+    title: '',
+    description: '',
+    text: {
+      data: ''
+    }
+  };
   path = '';
 
-  constructor(public http: Http, _params: RouteParams) {
+  constructor(public http: Http, _params: RouteParams,
+              private router: Router) {
     this.path = _params.get('1') || 'front-page';
   }
 
   ngOnInit() {
-    var url = 'http://castanyera.iskra.cat:8070/' + this.path;
+    var url = 'http://admin:admin@castanyera.iskra.cat:8070/' + this.path;
     var headers = new Headers();
     headers.append('Accept', 'application/json');
 
     this.http.get(url, {
       headers: headers
     }).subscribe(res => {
-      this.data = res.json();
+      this.model = res.json();
     });
+  }
+
+  onSave() {
+    var url = 'http://castanyera.iskra.cat:8070/' + this.path;
+    var headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', 'Basic ' + btoa('admin:admin'));
+
+    var body = JSON.stringify({
+      '@context': '/@@context.jsonld',
+      '@type': 'Document',
+      'title': this.model.title,
+      'description': this.model.description,
+      'text': this.model.text.data
+    });
+
+    this.http.patch(url, body, {
+      headers: headers
+    }).subscribe(res => {
+      this.router.navigateByUrl('/' + this.path);
+    });
+  }
+
+  onCancel() {
+    this.router.navigateByUrl('/' + this.path);
   }
 }
