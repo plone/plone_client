8a1efa645bd4c05bb705731934ab2b43cecd1dc1
Eric BREHAULT <ebrehault@gmail.com>
2016-05-16 18:43:29 +0200

    enable routerlnk in navbar

--- src/components/navigation/navigation.component.ts
+++ src/components/navigation/navigation.component.ts
@@ -1,16 +1,20 @@
 import {Component} from '@angular/core';
 import {ObjectService} from '../../services/object.service';
+import {Router} from '@angular/router';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'plone-navigation', // <app></app>
-  directives: [],
+  directives: [
+    ...ROUTER_DIRECTIVES,
+  ],
   providers: [ObjectService],
   template: require('./navigation.component.html')
 })
 export class Navigation {
   items = [];
 
-  constructor(private objectService: ObjectService) {}
+  constructor(private router: Router, private objectService: ObjectService) { }
 
   ngOnInit() {
     this.objectService.list('').subscribe(res => {
