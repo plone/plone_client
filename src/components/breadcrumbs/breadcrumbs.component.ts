2075822eb936356ecc724b0677f6fc9008b743b1
Sam Schwartz <obct537@gmail.com>
2016-05-17 03:30:01 -0500

    Updated breadcrumbs to use API

--- src/components/breadcrumbs/breadcrumbs.component.ts
+++ src/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,11 +1,34 @@
 import {Component} from '@angular/core';
+import {ObjectService} from '../../services/object.service';
 
 @Component({
   selector: 'plone-breadcrumbs', // <app></app>
   directives: [],
+  providers: [ObjectService],
   template: require('./breadcrumbs.component.html')
 })
 export class Breadcrumbs {
-  constructor() { }
+  objectService: ObjectService;
+  show = false;
+  crumbs = [];
 
+  constructor(objectService: ObjectService) {
+    this.objectService = objectService;
+  }
+
+  ngOnInit() {
+    this.objectService.get('/components_/breadcrumbs').subscribe(res => {
+
+      this.crumbs = res.json().data.items;
+      if( this.crumbs.length > 0 ) {
+
+        this.show = true;
+
+        this.crumbs.unshift({
+          'label': 'Home',
+          'uri': '/front-page'
+        });
+      }
+    });
+  }
 }
