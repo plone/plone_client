0b76dc64ba365bad4caeb5b08377c0a266357206
vangheem <vangheem@gmail.com>
2016-05-17 16:07:29 +0200

    get data correctly

--- src/components/breadcrumbs/breadcrumbs.component.ts
+++ src/components/breadcrumbs/breadcrumbs.component.ts
@@ -24,7 +24,7 @@ export class Breadcrumbs {
       if(data instanceof Array){
         data = data[0];
       }
-      this.crumbs = res.json().data.items;
+      this.crumbs = data.data.items;
       if( this.crumbs.length > 0 ) {
 
         this.show = true;
