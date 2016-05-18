8dc3f2360bc5a8b01198468e78ca837d2a6febe9
vangheem <vangheem@gmail.com>
2016-05-18 13:18:02 +0200

    disable angular universal

--- src/server.ts
+++ src/server.ts
@@ -33,52 +33,47 @@ app.set('view engine', 'html');
 app.use(bodyParser.json());
 
 
-function ngApp(req, res) {
-  let baseUrl = '/';
-  let url = req.originalUrl || '/';
-
-  let config: ExpressEngineConfig = {
-    directives: [ AppWrapper ],
-    platformProviders: [
-      provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
-      provide(BASE_URL, {useValue: baseUrl}),
-    ],
-    providers: [
-      provide(REQUEST_URL, {useValue: url}),
-      NODE_ROUTER_PROVIDERS,
-      NODE_HTTP_PROVIDERS
-    ],
-    async: true,
-    preboot: false // { appRoot: 'app' } // your top level app component selector
-  };
-
-  res.render('index', config);
-}
-
-function indexFile(req, res) {
-  res.sendFile('/index.html', {root: __dirname});
-}
+// function ngApp(req, res) {
+//   let baseUrl = '/';
+//   let url = req.originalUrl || '/';
+//
+//   let config: ExpressEngineConfig = {
+//     directives: [ AppWrapper ],
+//     platformProviders: [
+//       provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
+//       provide(BASE_URL, {useValue: baseUrl}),
+//     ],
+//     providers: [
+//       provide(REQUEST_URL, {useValue: url}),
+//       NODE_ROUTER_PROVIDERS,
+//       NODE_HTTP_PROVIDERS
+//     ],
+//     async: true,
+//     preboot: false // { appRoot: 'app' } // your top level app component selector
+//   };
+//
+//   res.render('index', config);
+// }
+//
+// function indexFile(req, res) {
+//   res.sendFile('/index.html', {root: __dirname});
+// }
 
 // Serve static files
 app.use(express.static(ROOT, {index: false}));
 
 
-// Our API for demos only
-app.post('/login', (req, res) => {
-  if(req.body.username == 'admin' && req.body.password == 'admin'){
-    res.json({
-      success: true
-    });
-  }else{
-    res.json({
-      success: false
-    });
-  }
-});
+app.get('/', (req, res) => {
+  res.sendFile('/index.html', {root: __dirname});
+})
+app.get('*', (req, res) => {
+  res.sendFile('/index.html', {root: __dirname});
+})
+
 
 // Routes with html5pushstate
-app.use('/', ngApp);
-app.use('/login', ngApp);
+// app.use('/', ngApp);
+// app.use('/login', ngApp);
 
 // Server
 app.listen(3000, () => {
