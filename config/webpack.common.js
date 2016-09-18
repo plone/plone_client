0efd8b5f416ce10a2153a038b34378cda77cbe3d
Eric BREHAULT <ebrehault@gmail.com>
2016-09-18 21:28:43 +0200

    fix webpack commands

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -95,7 +95,6 @@ module.exports = {
   plugins: [
 
     new ForkCheckerPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(true),
     new webpack.optimize.CommonsChunkPlugin({
       name: ['polyfills', 'vendor'].reverse()
     }),
