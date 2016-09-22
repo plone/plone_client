d4684b6d44b8c28bbe3924da4684c826466b770e
Eric BREHAULT <ebrehault@gmail.com>
2016-09-22 19:23:49 +0200

    add widget provider for ng2 schema form

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router'
-import { SchemaFormModule } from 'angular2-schema-form';
+import { SchemaFormModule, WidgetRegistry } from 'angular2-schema-form';
 import { HttpModule } from '@angular/http';
 
 import { routing,
@@ -77,7 +77,8 @@ import { SearchService } from '../../services/search.service';
         ObjectService,
         ObjectUtility,
         LoginService,
-        SearchService
+        SearchService,
+        WidgetRegistry
     ],
     bootstrap: [AppWrapper],
 })
